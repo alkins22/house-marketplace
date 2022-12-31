33bb4103a5af3dd419819fb590e66451168878bd
alkins22 <jakealkins@msn.com>
2022-12-31 10:15:37 -0500

    Section 13

---
+++ firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDJXIKErcvfeOTjZyad8aOQmCL5LFeWHt0",
+  authDomain: "house-marketplace-app-70cb0.firebaseapp.com",
+  projectId: "house-marketplace-app-70cb0",
+  storageBucket: "house-marketplace-app-70cb0.appspot.com",
+  messagingSenderId: "282146427643",
+  appId: "1:282146427643:web:f69775b14771c5fae3565e",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
