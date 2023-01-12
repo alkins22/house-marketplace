1e88bb49a686c17cf2596483836e860791d3aeef
alkins22 <jakealkins@msn.com>
2023-01-12 08:16:29 -0500

    Section 16

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import Explore from "./pages/Explore";
 import Category from "./pages/Category";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
+import Contact from "./pages/Contact";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import EditListing from "./pages/EditListing";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
 import Navbar from "./components/Navbar";
@@ -20,6 +23,10 @@ function App() {
           <Route path="/" element={<Explore />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
           <Route path="/category/:categoryName" element={<Category />}></Route>
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          ></Route>
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />}></Route>
           </Route>
@@ -27,6 +34,11 @@ function App() {
           <Route path="/sign-up" element={<Signup />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/create-listing" element={<CreateListing />}></Route>
+          <Route
+            path="/edit-listing/:listingId"
+            element={<EditListing />}
+          ></Route>
+          <Route path="/contact/:landlordId" element={<Contact />}></Route>
         </Routes>
         <Navbar></Navbar>
       </Router>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -18,6 +18,7 @@ import ListingItem from "../components/ListingItem";
 function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
   const params = useParams();
 
@@ -38,6 +39,9 @@ function Category() {
         // Execute query
         const querySnap = await getDocs(q);
 
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
         let listings = [];
 
         querySnap.forEach((doc) => {
@@ -55,6 +59,42 @@ function Category() {
 
     fetchListings();
   }, []);
+
+  //Pagination load more
+  const onFetchMoreListings = async () => {
+    try {
+      // Get reference
+      const listingsRef = collection(db, "listings");
+
+      // Create query
+      const q = query(
+        listingsRef,
+        where("type", "==", params.categoryName),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(10)
+      );
+
+      // Execute query
+      const querySnap = await getDocs(q);
+
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      let listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings((prevState) => [...prevState, ...listings]);
+      setLoading(false);
+    } catch (error) {
+      toast.error("Could not get listings");
+    }
+  };
 
   return (
     <div className="category">
@@ -81,6 +121,15 @@ function Category() {
               ))}
             </ul>
           </main>
+
+          <br />
+          <br />
+
+          {lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>
+              Load More
+            </p>
+          )}
         </>
       ) : (
         <p>No listings for {params.categoryName}</p>
